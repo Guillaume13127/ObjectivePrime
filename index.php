d94dc424fd96816c7daf2efa1311d968695dd192
Guillaume Mariotti <mariotti.guillaume@gmail.com>
2016-06-29 22:37:06 +0200

    [UPD] index.php ajout du favicon

--- index.php
+++ index.php
@@ -3,12 +3,17 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title></title>
+	<title>Objective'Prime</title>
+
+	<!-- Favicon -->
+	<link rel="icon" type="image/png" href="favicon.png" />
+
+
+	<!-- CSS -->
 	<link rel="stylesheet" href="./css/main.css">
+
 </head>
 <body>
-	<div id="content">
-		
-	</div>
+
 </body>
 </html>
